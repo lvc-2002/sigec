d1f3b3464b2d7cca8fae16880cc44e7500a6e736
Lázaro <lazarovasconceloscoutinho@gmail.com>
2018-06-21 22:18:19 -0300

    Mapeando entidades

--- dao/sigec-dao/src/br/com/sigec/model/Cliente.java
+++ dao/sigec-dao/src/br/com/sigec/model/Cliente.java
@@ -2,14 +2,23 @@ package br.com.sigec.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Cliente implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue
 	private long id;
 	
 	private String nome;
 	
+	@OneToOne
 	private Contato contato;
 
 	public long getId() {
--- dao/sigec-dao/src/br/com/sigec/model/Contato.java
+++ dao/sigec-dao/src/br/com/sigec/model/Contato.java
@@ -2,10 +2,17 @@ package br.com.sigec.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Contato implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue
 	private long id;
 	
 	private String telefone;
